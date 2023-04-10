5d9a0ce6ad3471438bde47dd4729634e30096520
Amit-TheOne <amit.dew009@gmail.com>
2023-04-11 02:43:55 +0530

    context api initialized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,17 +7,20 @@ import {
 import { Home } from './components/Home';
 import { Navbar } from './components/Navbar';
 import { About } from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="about/*" element={<About />} />
-      </Routes>
-    </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about/*" element={<About />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
